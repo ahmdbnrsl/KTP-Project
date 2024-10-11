7f146def1ee64f560675c0b6e04382e12f26be8e
abrazax56 <ahmadbenirusli@gmail.com>
2024-10-11 13:01:30 +0700

    add background in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,122 +11,112 @@ import {
     SelectItem,
     SelectLabel,
     SelectTrigger,
-    SelectValue,
+    SelectValue
 } from '@/components/ui/select';
 import {
     Carousel,
     CarouselContent,
-    CarouselItem,
+    CarouselItem
 } from '@/components/ui/carousel';
 import Autoplay from 'embla-carousel-autoplay';
 import Link from 'next/link';
 
 export default function LoginPage() {
     return (
-        <section className="w-full min-h-screen relative flex flex-col justify-center items-center">
-            <Image
-                src="https://img.freepik.com/free-photo/abstract-blue-geometric-shapes-background_24972-1841.jpg?t=st=1728616015~exp=1728619615~hmac=ac681583930fc2802a7913f2a1b7e5edb9afee301f968b15e017a427ee97b026&w=740"
-                width={1200}
-                height={1200}
-                className="w-full h-screen object-cover"
-                loading="lazy"
-                alt="bg"
-            />
-            <main className="w-full min-h-screen bg-zinc-100 flex flex-col justify-center items-center absolute z-[10]">
-                <div className="w-full max-w-md py-6 px-5 rounded-xl bg-zinc-100 border-2 border-zinc-300 shadow-2xl shadow-zinc-500 lg:flex lg:flex-row-reverse lg:max-w-5xl lg:p-0">
-                    <div className="flex justify-center w-full lg:w-2/3 border-l-2 border-zinc-300">
-                        <Image
-                            src="/logo/auth.png"
-                            alt="logo"
-                            width={960}
-                            height={540}
-                            className="w-2/3 object-cover"
-                            loading="lazy"
-                        />
-                    </div>
-                    <hr className="mt-6 lg:hidden"></hr>
-                    <form className="mt-3 mb-5 lg:w-1/3 flex flex-col p-5">
-                        <p className="px-3 text-sm font-semibold text-zinc-800">
-                            NIM/NIS
-                        </p>
-                        <div className="flex flex-col mt-3 relative">
-                            <Input
-                                type="number"
-                                name="nim"
-                                id="nim"
-                                className="peer placeholder:text-transparent px-4 py-3 text-zinc-800"
-                                placeholder="nim"
-                            ></Input>
-                            <label
-                                htmlFor="nim"
-                                className="text-xs text-zinc-600 bg-zinc-100 ml-3 px-2 absolute -translate-y-3 peer-placeholder-shown:translate-y-3 font-normal peer-focus:-translate-y-3 w-auto py-1 peer-focus:text-zinc-800 -mt-1"
-                            >
-                                Masukan NIM/NIS Anda disini.
-                            </label>
-                        </div>
-                        <p className="px-3 text-sm text-zinc-800 font-semibold mt-3">
-                            Password
-                        </p>
-                        <div className="flex flex-col mt-3 relative">
-                            <Input
-                                type="password"
-                                name="password"
-                                id="password"
-                                className="peer placeholder:text-transparent px-4 py-3 text-zinc-800"
-                                placeholder="password"
-                            ></Input>
-                            <label
-                                htmlFor="password"
-                                className="text-xs text-zinc-600 bg-zinc-100 ml-3 px-2 absolute -translate-y-3 peer-placeholder-shown:translate-y-3 font-normal peer-focus:-translate-y-3 w-auto py-1 peer-focus:text-zinc-800 -mt-1"
-                            >
-                                Masukan Password Anda disini.
-                            </label>
-                        </div>
-                        <p className="px-3 text-sm text-zinc-800 font-semibold mt-3">
-                            Status
-                        </p>
-                        <div className="flex flex-col mt-3 relative">
-                            <Select>
-                                <SelectTrigger className="w-full text-xs">
-                                    <SelectValue
-                                        className="text-xs"
-                                        placeholder="Pilih status Anda"
-                                    />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectGroup>
-                                        <SelectLabel>Status</SelectLabel>
-                                        <SelectItem value="Mahasiswa KTP">
-                                            Mahasiswa KTP
-                                        </SelectItem>
-                                        <SelectItem value="Siswa STP">
-                                            Siswa STP
-                                        </SelectItem>
-                                        <SelectItem value="Admin / Pengurus">
-                                            Admin / Pengurus
-                                        </SelectItem>
-                                    </SelectGroup>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <Button
-                            type="submit"
-                            className="mt-6 w-full flex justify-center items-center gap-2"
-                        >
-                            <FaUserLock className="-mt-1" /> Masuk
-                        </Button>
-                        <p className="text-sm text-zinc-800 mt-5 w-full text-center">
-                            Belum punya akun?{' '}
-                            <Link
-                                href="/"
-                                className="font-semibold cursor-pointer"
-                            >
-                                Hubungi admin
-                            </Link>
-                        </p>
-                    </form>
+        <main className='w-full min-h-screen bg-zinc-100 flex flex-col justify-center items-center p-6 bg-zinc-100 bg-wp bg-no-repeat bg-cover bg-right'>
+            <div className='w-full max-w-md py-6 px-5 rounded-xl bg-zinc-100 border-2 border-zinc-300 shadow-2xl shadow-zinc-500 lg:flex lg:flex-row-reverse lg:max-w-5xl lg:p-0'>
+                <div className='flex justify-center w-full lg:w-2/3 lg:border-l-2 lg:border-zinc-300'>
+                    <Image
+                        src='/logo/auth.png'
+                        alt='logo'
+                        width={960}
+                        height={540}
+                        className='w-2/3 object-cover'
+                        loading='lazy'
+                    />
                 </div>
-            </main>
-        </section>
+                <hr className='lg:hidden'></hr>
+                <form className='mt-3 mb-5 lg:w-1/3 flex flex-col p-5'>
+                    <p className='px-3 text-sm font-semibold text-zinc-800'>
+                        NIM/NIS
+                    </p>
+                    <div className='flex flex-col mt-3 relative'>
+                        <Input
+                            type='number'
+                            name='nim'
+                            id='nim'
+                            className='peer placeholder:text-transparent px-4 py-3 text-zinc-800'
+                            placeholder='nim'
+                        ></Input>
+                        <label
+                            htmlFor='nim'
+                            className='text-xs text-zinc-600 bg-zinc-100 ml-3 px-2 absolute -translate-y-3 peer-placeholder-shown:translate-y-3 font-normal peer-focus:-translate-y-3 w-auto py-1 peer-focus:text-zinc-800 -mt-1'
+                        >
+                            Masukan NIM/NIS Anda disini.
+                        </label>
+                    </div>
+                    <p className='px-3 text-sm text-zinc-800 font-semibold mt-3'>
+                        Password
+                    </p>
+                    <div className='flex flex-col mt-3 relative'>
+                        <Input
+                            type='password'
+                            name='password'
+                            id='password'
+                            className='peer placeholder:text-transparent px-4 py-3 text-zinc-800'
+                            placeholder='password'
+                        ></Input>
+                        <label
+                            htmlFor='password'
+                            className='text-xs text-zinc-600 bg-zinc-100 ml-3 px-2 absolute -translate-y-3 peer-placeholder-shown:translate-y-3 font-normal peer-focus:-translate-y-3 w-auto py-1 peer-focus:text-zinc-800 -mt-1'
+                        >
+                            Masukan Password Anda disini.
+                        </label>
+                    </div>
+                    <p className='px-3 text-sm text-zinc-800 font-semibold mt-3'>
+                        Status
+                    </p>
+                    <div className='flex flex-col mt-3 relative'>
+                        <Select>
+                            <SelectTrigger className='w-full text-xs'>
+                                <SelectValue
+                                    className='text-xs'
+                                    placeholder='Pilih status Anda'
+                                />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Status</SelectLabel>
+                                    <SelectItem value='Mahasiswa KTP'>
+                                        Mahasiswa KTP
+                                    </SelectItem>
+                                    <SelectItem value='Siswa STP'>
+                                        Siswa STP
+                                    </SelectItem>
+                                    <SelectItem value='Admin / Pengurus'>
+                                        Admin / Pengurus
+                                    </SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <Button
+                        type='submit'
+                        className='mt-6 w-full flex justify-center items-center gap-2'
+                    >
+                        <FaUserLock className='-mt-1' /> Masuk
+                    </Button>
+                    <p className='text-sm text-zinc-800 mt-5 w-full text-center'>
+                        Belum punya akun?{' '}
+                        <Link
+                            href='/'
+                            className='font-semibold cursor-pointer'
+                        >
+                            Hubungi admin
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </main>
     );
 }
