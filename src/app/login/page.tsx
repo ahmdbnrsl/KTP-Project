8f169949feddfc363324ae7826e5c72f49e7f4ce
abrazax56 <ahmadbenirusli@gmail.com>
2024-10-18 05:10:49 +0700

    add background in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -93,7 +93,8 @@ export default function LoginPage({ searchParams }: any) {
             });
             const { push } = useRouter();
             if (!login?.error) {
-                push('/dashboard');
+                //push('/dashboard');
+                alert('success login');
             } else if (login.status === 401) {
                 throw new Error('NIM/NIS atau password salah!');
             }
